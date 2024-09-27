dcf2e5fdab83581bc8265bf07632e0d491eaf189
Cassia072007 <00001105306604SP@al.educacao.sp.gov.br>
2024-09-27 12:46:24 -0300

    projeto final

---
+++ script.js
@@ -0,0 +1,11 @@
+const avança = document.querySelectorA11('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
